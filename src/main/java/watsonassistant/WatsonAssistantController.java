21a11b186a5e7f47ee38ca4773cf0ddcdb7d1b0f
DAVID JESUS GALLARDO PARRA <0010528@insags.com>
2020-03-03 14:31:05 +0100

    Primer commit

---
+++ src/main/java/watsonassistant/WatsonAssistantController.java
@@ -0,0 +1,15 @@
+package watsonassistant;
+
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+public class WatsonAssistantController {
+	
+	@RequestMapping(value = "/send",method = RequestMethod.GET)
+	
+	public String send (@RequestParam("message") String message) {
+		
+		return message;
+	}
+
+}
